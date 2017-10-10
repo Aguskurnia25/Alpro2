90c74d1d52ab18f80cf80a0cf40bbf87aa11682f
Agus kurnia <32480421+Aguskurnia25@users.noreply.github.com>
2017-10-10 10:44:31 +0700

    Pertemuan ke 2
    Alpro 2

---
+++ masaknasi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+cout << "==========================";
+cout << endl;
+cout << "CARA MEMASAK NASI";
+cout << endl;
+cout << "==========================";
+cout << endl;
+cout << "1. ambil wadah";
+cout << endl;
+cout << "2. masukan beras yang sudah disipkan kedalam wadah";
+cout << endl;
+cout << "3. masukan air kedalam wadah yang berisi beras";
+cout << endl;
+cout << "4. cuci beras hingga bersih";
+cout << endl;
+cout << "5. buang air hasil cucian beras";
+cout << endl;
+cout << "6. masukan beras yang sudah bersih kedalam rice cooker";
+cout << endl;
+cout << "7. tambahkan air bersih kedalam rice cooker secukupnya";
+cout << endl;
+cout << "8. tutup rice cooker dan nyalakan";
+cout << endl;
+cout << "9. tunggu hingga matang atau lampu indikator rice cooker menyala";
+cout << endl;
+cout << "10. bila sudah matang nasi pun siap di hidangkan";
+cout << endl;
+return 0;
+}
---
+++ nama.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main () {
+string nama;
+cout << "masukan nama anda : ";
+cin >> nama;
+cout << "nama anda : " + nama;
+return 0;
+}
