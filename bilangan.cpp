e25c86846973e52620ad7485011c6e624a677a78
Agus kurnia <32480421+Aguskurnia25@users.noreply.github.com>
2017-10-18 03:59:56 +0700

    Alpro 2
    Pertemuan ke 4

---
+++ bilangan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+int a, b;
+cout << "masukan nilai a : ";
+cin >> a;
+cout << "masukan nilai b : ";
+cin >> b;
+if (a > b) {
+ cout << "maka nilai terbesar adalah : " <<a ;
+}
+else {
+cout << "maka nilai terbesar adalah : " <<b ;}
+return 0;
+}
