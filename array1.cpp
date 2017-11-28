1574d210cd0c9962c025a4e47fd7398860250107
Agus kurnia <32480421+Aguskurnia25@users.noreply.github.com>
2017-11-28 22:20:34 +0700

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+string nama1, nama2, nama3;
+
+cin >> nama1
+cin >> nama2
+cin >> nama3
+
+cout << nama1 << endl;
+cout << nama2 << endl;
+cout << nama3 << endl;
+
+return 0;
+
+}
